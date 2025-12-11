62772a7362d8e1160850c5e0515b659ec38ce682
Kidron Filbrun <kidron.filbrun@horizon3.ai>
2025-12-11 11:34:15 -0500

    Added claude install

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo apt install vim xxd 
 curl -sS https://starship.rs/install.sh >> /tmp/st.sh
 sh /tmp/st.sh --yes
+curl -fsSL https://claude.ai/install.sh | bash
 cat bashrc.additions >> ~/.bashrc
